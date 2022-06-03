bfe1d15312071fba9f9f2ef1763cabc32b52f3e7
Ty Morrow <morrowtym@gmail.com>
2022-06-02 23:30:40 -0500

    snek test

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const { Client, Collection, Intents } = require("discord.js");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const { token, owner, client_id, test_guild_id } = require("./config.json");
-const { snekfetch } = require('snekfetch');
+const { snek } = require('snekfetch');
 global.adminIDs = [owner, "178273444041981952"];
 global.roleID = "980346316720857100";
 
--- interactions/buttons/category/butt.js
+++ interactions/buttons/category/butt.js
@@ -4,8 +4,6 @@
  * @since 3.0.0
  */
 
- const snekfetch = require('snekfetch');
-
 module.exports = {
 	id: "butt",
 
@@ -17,7 +15,7 @@ module.exports = {
 
 	async execute(interaction) {
 		await interaction.reply({
-			x : snekfetch.get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week').query({ limit: 800 }),
+			x : snek.get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week').query({ limit: 800 }),
             allowed : message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18),
             randomnumber : Math.floor(Math.random() * allowed.length),
             embed : new Discord.RichEmbed().setColor(0x00A2E8).setTitle(allowed[randomnumber].data.title).setDescription("Posted by: " + allowed[randomnumber].data.author).setImage(allowed[randomnumber].data.url).addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments).setFooter("Memes provided by r/dankmemes"),
