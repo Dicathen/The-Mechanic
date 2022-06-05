260bdb4d8872a7459dcccff9f75567115e2c4834
Ty Morrow <morrowtym@gmail.com>
2022-06-04 22:33:10 -0500

    Update d20.js

--- interactions/buttons/dice/d20.js
+++ interactions/buttons/dice/d20.js
@@ -15,7 +15,7 @@ module.exports = {
 
 	async execute(interaction) {
 		await interaction.reply({
-			content: "You rolled a D20 and got a " + String(Number(Math.random * 20 + 1)) + "!",
+			content: "You rolled a D20 and got a " + String(1) + "!",
 		});
 		return;
 	},
