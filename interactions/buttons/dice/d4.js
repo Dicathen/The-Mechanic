d1bc823eb091c317d5edd3acb47fb06b51d489f5
Ty Morrow <morrowtym@gmail.com>
2022-06-04 23:03:34 -0500

    Update d4.js

--- interactions/buttons/dice/d4.js
+++ interactions/buttons/dice/d4.js
@@ -4,7 +4,7 @@ module.exports = {
 	async execute(interaction) {
 		var roll = Math.floor(Math.random() * 4) + 1;
 		await interaction.reply({
-			content: "You rolled a D4 and got a **" + String(roll) + "**!",
+			content: interaction.user + " You rolled a D4 and got a **" + String(roll) + "**!",
 		});
 		return;
 	},
