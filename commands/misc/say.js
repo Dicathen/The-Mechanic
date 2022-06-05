34b0fad97f2f7658c6c1d915b65d3bd8b4d20436
Ty Morrow <morrowtym@gmail.com>
2022-06-04 20:59:48 -0500

    Update say.js

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -3,13 +3,17 @@ module.exports = {
 	description: "Says something",
 
 	execute(message, args) {
-		message.channel.send({ content: toString(args)});
 		message.channel.messages.fetch({ limit: 1 }).then(messages => {
 			let lastMessage = messages.first();
 			
 			if (!lastMessage.author.bot) {
 			  lastMessage.delete();
 			}
-		  });
+		});
+		const messageText = "";
+		args.forEach(element => {
+			messageText += element + " ";
+		});
+		message.channel.send({ content: messageText});
 	},
 };
