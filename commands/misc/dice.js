2939f6ad0ddbe9e80fd11ac15b41651c616b2093
Ty Morrow <morrowtym@gmail.com>
2022-06-04 21:58:31 -0500

    Create dice.js

---
+++ commands/misc/dice.js
@@ -0,0 +1,18 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+	name: "dice",
+	description: "Roll a die",
+
+	execute(message, args) {
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Primary')
+					.setStyle('PRIMARY'),
+			);
+
+		await interaction.reply({ content: 'Pong!', components: [row] });
+	},
+};
