bba668b4c8d95edbd924f55f635e5ee7bec66913
Ty Morrow <morrowtym@gmail.com>
2022-06-05 16:02:12 -0500

    Create cat.js

---
+++ commands/misc/cat.js
@@ -0,0 +1,37 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+function image(message){
+    var options = {
+        url: 'http://results.dogpile.com/serp?qc=images&q=' + srch,
+        method: 'GET',
+        headers: {
+            'Accept': 'text/html',
+            'User-Agent': 'Chrome'
+        }
+        
+    };
+    request(options, function(error, response, responseBody){
+        if (error){
+            return;
+        }
+        $ = cheerio.load(responseBody);
+        var links = $('.image a.link');
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr('href'));
+        console.log(urls);
+        if (!urls.length){
+            return;
+        }
+        message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
+    });
+}
+
+module.exports = {
+	name: "cat",
+	description: "Cat picture",
+
+	execute(message, args) {
+		
+		message.channel.send({ content: image("cat")});
+	},
+};
