2ed7a4b40206a6dec29835041ad9d1f9017f370c
Ty Morrow <morrowtym@gmail.com>
2022-06-05 16:40:25 -0500

    remove cheerio

--- commands/misc/cat.js
+++ commands/misc/cat.js
@@ -1,37 +1,8 @@
-const cheerio = require('cheerio');
-const request = require('request');
-
-function image(message){
-    var options = {
-        url: 'http://results.dogpile.com/serp?qc=images&q=' + 'cat',   // Cat is placeholder for variable in search
-        method: 'GET',
-        headers: {
-            'Accept': 'text/html',
-            'User-Agent': 'Chrome'
-        }
-        
-    };
-    request(options, function(error, response, responseBody){
-        if (error){
-            return;
-        }
-        $ = cheerio.load(responseBody);
-        var links = $('.image a.link');
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr('href'));
-        console.log(urls);
-        if (urls.length == 0){
-            return;
-        }
-        message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
-    });
-}
-
 module.exports = {
 	name: "cat",
 	description: "Cat picture",
 
 	execute(message, args) {
-		image(message);
-		//message.channel.send({ content: image("cat")});
+		message.channel.send({ content: "cat"});
 	},
 };
