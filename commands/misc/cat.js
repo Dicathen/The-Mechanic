f51b514970a5b348d2c0c5486986c59f0c020c74
Ty Morrow <morrowtym@gmail.com>
2022-06-05 16:46:32 -0500

    Update cat.js

--- commands/misc/cat.js
+++ commands/misc/cat.js
@@ -1,8 +1,14 @@
+const ImageSearch = require('free-google-image-search');
+
 module.exports = {
 	name: "cat",
 	description: "Cat picture",
 
 	execute(message, args) {
+		GoogleImageSearch.searchImage("cat")
+		.then((res) => {
+    		console.log(res); // This will return array of image URLs
+		})
 		message.channel.send({ content: "cat"});
 	},
 };
