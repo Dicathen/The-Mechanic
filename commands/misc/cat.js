e339ca0b01f89abd10b797ebb787b53fed81390c
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2022-06-10 23:13:23 -0500

    Update cat.js

--- commands/misc/cat.js
+++ commands/misc/cat.js
@@ -1,11 +1,11 @@
-const search = require('reddit-random-image');
+const { search} = require('reddit-random-image');
 
 module.exports = {
 	name: "cat",
 	description: "Cat picture",
 
 	execute(message, args) {
-		const image = search.search('CatGifs');
+		const image = search('CatGifs');
 		message.channel.send({ content: image});
 	},
 };
