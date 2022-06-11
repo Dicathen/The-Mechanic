6c03989172d1795781edcf0591e327739c2393a3
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2022-06-10 23:45:08 -0500

    Update cat.js

--- commands/misc/cat.js
+++ commands/misc/cat.js
@@ -1,11 +1,17 @@
 const { getPost, getImage} = require('random-reddit');
 
+async function redditImage() {
+	const post = await getPost('cat');
+	const image = await getImage(post.url);
+	return image;
+}
+
 module.exports = {
 	name: "cat",
 	description: "Cat picture",
 
 	execute(message, args) {
-		const image = await Promise.resolve(getImage('cat'));
+		const image = redditImage();
 		message.channel.send("GATO", { files: [image] });
 		console.log(image);
 	},
