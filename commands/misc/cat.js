b61995e8c0061597ea7af9ad72a802a33d89aafe
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2022-06-10 23:18:10 -0500

    random redd

--- commands/misc/cat.js
+++ commands/misc/cat.js
@@ -1,11 +1,11 @@
-const { search} = require('reddit-random-image');
+const { getPost, getImage} = require('random-reddit');
 
 module.exports = {
 	name: "cat",
 	description: "Cat picture",
 
 	execute(message, args) {
-		const image = search('CatGifs');
+		const image = getImage('cats');
 		message.channel.send({ content: image});
 	},
 };
