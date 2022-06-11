8c51b0b9ff26c0b5ea598e75bec4624ec5fbd07e
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2022-06-11 00:09:13 -0500

    reddit!

---
+++ commands/misc/reddit.js
@@ -0,0 +1,24 @@
+const { getImage } = require('random-reddit');
+
+async function redditImage(messageText) {
+	const image = await getImage(messageText);
+	return image;
+}
+
+async function sendImage(message, messageText) {
+	const image = await redditImage(messageText);
+	return message.channel.send(image);
+}
+
+module.exports = {
+	name: "reddit",
+	description: "Sends a random image from a specified subreddit",
+
+	execute(message, args) {
+		var messageText = "";
+		for(var i = 0; i < args.length; i++) {
+			messageText += args[i] + " ";
+		}
+		sendImage(message);
+	},
+};
