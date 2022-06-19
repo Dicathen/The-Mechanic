447cced4d9bfab43e73340ca49de4147e229a143
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-06-19 12:34:13 -0500

    Update reddit.js

--- commands/misc/reddit.js
+++ commands/misc/reddit.js
@@ -29,7 +29,7 @@ async function redditPost(message, messageText) {
 async function sendImage(message, messageText) {
 	const image = await redditPost(message, messageText);
 	if(image.url == "" || image === undefined) {
-		return message.channel.send("This subreddit does not exist or is NSFW.");
+		return message.channel.send("This subreddit does not exist or is NSFW or something went terribly wrong.");
 	}
 	else {
         if(message.channel.nsfw == true)
