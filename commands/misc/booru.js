c0820896fe7586d510d024034bf16b6ffed59817
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2022-06-20 01:57:48 -0500

    Update booru.js

--- commands/misc/booru.js
+++ commands/misc/booru.js
@@ -2,7 +2,7 @@ const boo = require('booru')
 
 function getBooru(message, site, term) {
 	//const sb = Booru.forSite('sb')
-	if((boo.sites).includes(site) == false) {
+	if(boo.sites.getBooru(site) == undefined || (boo.sites.getBooru(site).nsfw == false && message.channel.nsfw == true)) {
 		return message.channel.send("This site does not exist or is NSFW. Use this command with the following\nSFW: e926, konan, safebooru, tbib\nNSFW: e621, hypnohub, danbooru, konac, yandere, gelbooru, rule34, xbooru, paheal, derpibooru, realbooru");
 	}
 	boo.search(site, [term], { limit: 3, random: true }).then(
