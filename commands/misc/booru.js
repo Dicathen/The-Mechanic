419bba8c7b243ba42ce0633abfc63c0bb7f6f29c
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2022-06-20 14:28:43 -0500

    Update booru.js

--- commands/misc/booru.js
+++ commands/misc/booru.js
@@ -13,6 +13,12 @@ async function getBooru(message, site, term) {
 	catch(err){
 		return message.channel.send("*This site does not exist. Use this command with the following:*\n*SFW:** e926, konan, safebooru, tbib\n**NSFW:** e621, hypnohub, danbooru, konac, yandere, gelbooru, rule34, xbooru, paheal, derpibooru, realbooru*");
 	}
+	boo.search('safebooru', ['glaceon'], { limit: 3, random: true }).then(
+		posts => {
+		  //for (let post of posts) console.log(post.fileUrl, post.postView)
+		  image = posts[0];
+		},
+	  )
 	console.log(image);
 	if(image.booru.nsfw == true && message.channel.nsfw == false){
 		return message.channel.send("*This site is NSFW. Use this command with the following:*\n**SFW:** e926, konan, safebooru, tbib\n**NSFW:** e621, hypnohub, danbooru, konac, yandere, gelbooru, rule34, xbooru, paheal, derpibooru, realbooru");
