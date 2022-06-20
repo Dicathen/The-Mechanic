7311efbbcd66cc65577d34481f9ee5a9fef576a6
Ty Morrow <57633827+TMMorrow@users.noreply.github.com>
2022-06-20 02:27:33 -0500

    Update booru.js

--- commands/misc/booru.js
+++ commands/misc/booru.js
@@ -12,7 +12,8 @@ async function getBooru(message, site, term) {
 		return message.channel.send("*This site is NSFW. Use this command with the following:*\n**SFW:** e926, konan, safebooru, tbib\n**NSFW:** e621, hypnohub, danbooru, konac, yandere, gelbooru, rule34, xbooru, paheal, derpibooru, realbooru");
 	}
 	const image = await website.search((term), {limit:2});
-	return message.channel.send(image);
+	console.log(image);
+	return message.channel.send("image");
 }
 
 module.exports = {
