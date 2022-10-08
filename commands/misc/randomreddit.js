ab1e6df26ab7eeec6cd2faf5fa1dba5aacc8d289
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-07 21:53:23 -0700

    Create randomreddit.js

---
+++ commands/misc/randomreddit.js
@@ -0,0 +1,73 @@
+const { getPost } = require('random-reddit');
+
+async function redditPost(message, messageText) {
+	var nsfw = false;
+	if(message.channel.nsfw == true){
+		nsfw = true;
+	}
+	let options = {
+		imageOnly: true,
+		allowNSFW: nsfw
+	};
+	try {
+		const image = await getPost("randnsfw", options);
+		if(image.over_18 && message.channel.nsfw == false) {
+			return "";
+		}
+		else {
+			return image;
+		}
+	}
+	catch(err) {
+		return "";
+	}
+    
+}
+
+async function sendImage(message, messageText) {
+	const image = await redditPost(message, messageText);
+    console.log(image);
+	if(image.url === undefined || image === undefined) {
+		return message.channel.send("*This subreddit does not exist, is NSFW, or something went terribly wrong.*");
+	}
+	else {
+
+        if(image.is_video == true) {
+            return message.channel.send(`https://reddit.com${image.permalink}`);
+        }
+        else if(image.is_gallery == true) {
+            let messageContent = "";
+            image.gallery_data.items.forEach(item => {
+                messageContent = messageContent + `https://i.redd.it/${item.media_id}.jpg \n`;
+            });
+            return message.channel.send(messageContent);
+        }
+        else if(image.url.includes("imgur") || image.url.includes("redd")) {
+            return message.channel.send(image.subreddit + " \n" + image.title + " \n" + image.url);
+        }
+        else if(image.url.includes("gfycat")) {
+            sendImage(message, messageText);
+            return;
+        }
+        else {
+            return message.channel.send(image.subreddit + " \n" + image.title + " \n" + image.url);
+        }
+	}
+}
+
+module.exports = {
+	name: "randomreddit",
+	aliases: ["rand", "rr"],
+	description: "Sends a random image from a specified subreddit",
+
+	execute(message, args) {
+		var messageText = "cats";
+		if(args.length > 0) {
+			var messageText = String(args[0]);
+		}
+		sendImage(message, messageText);
+		if(message.channel.nsfw == true) {
+            message.delete();
+        }
+	},
+};
