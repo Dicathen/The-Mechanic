3830843823ba0a63d303cb2c48a30005a34f9505
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-15 18:45:46 -0700

    s

--- commands/misc/randommessage.js
+++ commands/misc/randommessage.js
@@ -1,6 +1,5 @@
-import { Snowflake } from "@lakuna/trigger";
 const DateGenerator = require('random-date-generator');
-
+const EPOCH = 1420070400000;
 module.exports = {
 	name: "randommessage",
 	aliases: ["rm", "randmes"],
@@ -12,9 +11,16 @@ module.exports = {
         let endDate = new Date(); // should be current date
         console.log(endDate.toDateString())
 
-        message.channel.messages.fetch(Snowflake.fromDate(DateGenerator.getRandomDateInRange(startDate, endDate)))
+        message.channel.messages.fetch(fromDate(DateGenerator.getRandomDateInRange(startDate, endDate)))
         .then(m => message.channel.send(m.author.username + ": \"" + m.toString() + "\""))
         .catch(console.error);
 	},
 };
 
+function fromDate(date) {
+    return new Snowflake(`0b${BigInt(date.getTime() - EPOCH) // Translate the date by the Discord epoch.
+        .toString(2) // Convert the date to binary.
+        .padStart(0x2a, "0") // Pad the start until the timestamp is the correct length.
+        .padEnd(0x40, "0") // Pad the end until the snowflake is the correct length.
+    }`);
+}
