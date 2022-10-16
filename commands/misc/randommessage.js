acf0c971e8938da518744fcfb549b02fee4e216d
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-15 17:17:05 -0700

    Update randommessage.js

--- commands/misc/randommessage.js
+++ commands/misc/randommessage.js
@@ -1,18 +1,31 @@
 const fs = require('fs');
+const fetcher = new Fetcher(client);
+let insearch = false;
 module.exports = {
 	name: "randommessage",
 	aliases: ["rm", "randmes"],
 	description: "Sends a random message from a the discord",
 
 	async execute(message, args) {
-        message.channel.send("Trying to fetch all messages please don't send me another command.");
+
+
+        if(insearch)
+        {
+            return message.channel.send("Bro I am literally storing all 360000+ messages rn stop");
+        }
+
+        message.channel.send("Trying to fetch all messages please don't run this command again.");
 
         var file = fs.createWriteStream('messages.txt')
         file.on('error', function(err) { /* error handling */ });
-        await message.channel.messages.fetch({ limit: 10 }).then(messages => {
+
+        insearch = true;
+        await fetcher.fetchChannel(message.channel).then(messages => {
             messages.forEach(function(m) { 
                 file.write(m.id + "\n");
             });
+
+            message.channel.send("Done.")
         })
 
         file.end();
@@ -20,3 +33,7 @@ module.exports = {
         //return message.channel.send("Message: " + messages[Math.floor(Math.random()*messages.length)].toString());
 	},
 };
+
+fetcher.on('fetchChannel', async channel => {
+	await message.channel.send(`Fetching <#${channel.id}>.`);
+});
