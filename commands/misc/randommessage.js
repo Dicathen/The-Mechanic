b50b6aa36a3b4ac8cdc8040d0fa7f193aeb831ca
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-15 18:15:45 -0700

    Update randommessage.js

--- commands/misc/randommessage.js
+++ commands/misc/randommessage.js
@@ -13,7 +13,7 @@ module.exports = {
         let endDate = new Date(); // should be current date
         console.log(endDate.toDateString())
 
-        message.channel.messages.fetch(Snowflake.fromDate(DateGenerator.getRandomDateInRange(startDate, endDate)))
+        message.channel.messages.fetch(new Snowflake().fromDate(DateGenerator.getRandomDateInRange(startDate, endDate)))
         .then(m => message.channel.send(m.author.username + ": \"" + m.toString() + "\""))
         .catch(console.error);
 	},
