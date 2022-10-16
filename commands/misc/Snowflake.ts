d9b41703e0eb71e159727fefd7f65d476e7298f8
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-15 18:05:22 -0700

    s

---
+++ commands/misc/Snowflake.ts
@@ -0,0 +1,54 @@
+/** A uniquely identifiable descriptor of a thing in Discord. */
+export class Snowflake {
+  /** The Discord epoch (the first millisecond of 2015). */
+  public static EPOCH = 1420070400000;
+
+  /**
+   * Creates a snowflake.
+   * @param value The unsigned 64-bit integer that represents the snowflake.
+   */
+  public constructor(value: string | number | bigint) {
+    this.value = BigInt(value);
+  }
+
+  /**
+   * Creates a snowflake from a date.
+   * @param date The date.
+   * @returns The snowflake.
+   */
+  public static fromDate(date: Date): Snowflake {
+    return new Snowflake(`0b${
+      BigInt(date.getTime() - Snowflake.EPOCH) // Translate the date by the Discord epoch.
+        .toString(2) // Convert the date to binary.
+        .padStart(0x2a, "0") // Pad the start until the timestamp is the correct length.
+        .padEnd(0x40, "0") // Pad the end until the snowflake is the correct length.
+    }`);
+  }
+
+  /** The increment value of this snowflake. */
+  public get increment(): number {
+    return Number(this.value & 0xfffn);
+  }
+
+  /** The process value of this snowflake. */
+  public get process(): number {
+    return Number((this.value & 0x1f000n) >> 12n);
+  }
+
+  /** The timestamp value of this snowflake. */
+  public get timestamp(): Date {
+    return new Date(Number((this.value >> 22n) + BigInt(Snowflake.EPOCH)));
+  }
+
+  public readonly value: bigint;
+
+  /** The worker value of this snowflake. */
+  public get worker(): number {
+    return Number((this.value & 0x3e0000n) >> 17n);
+  }
+
+  /** Converts this snowflake to a string. */
+  public toString(): string {
+    return this.value.toString();
+  }
+}
--- commands/misc/randommessage.js
+++ commands/misc/randommessage.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
-const fetchAll = require('discord-fetch-all');
- 
-let insearch = false;
+import {Snowflake} from './Snowflake.js';
+let DateGenerator = require('random-date-generator');
+
 module.exports = {
 	name: "randommessage",
 	aliases: ["rm", "randmes"],
@@ -9,36 +8,13 @@ module.exports = {
 
 	async execute(message, args) {
 
+        let startDate = new Date(2020, 10, 15);
+        let endDate = new Date(); // should be current date
+        console.log(endDate.toDateString())
 
-        if(insearch)
-        {
-            return message.channel.send("Bro I am literally storing all 360000+ messages rn stop");
-        }
-
-        message.channel.send("Trying to fetch all messages please don't run this command again.");
-
-        var file = fs.createWriteStream('messages.txt')
-        file.on('error', function(err) { /* error handling */ });
-
-        insearch = true;
-
-        // First parameter needs to be a discord.js channel object
-        // Second parameter is a optional set of options.
-        await fetchAll.messages(message.channel, {
-            reverseArray: false, // Reverse the returned array
-            userOnly: true, // Only return messages by users
-            botOnly: false, // Only return messages by bots
-            pinnedOnly: false, // Only returned pinned messages
-        }).then(messages => {
-            messages.forEach(function(m) { 
-                file.write(m.id + "\n");
-            });
-
-            message.channel.send("Done.")
-        });
-
-        file.end();
-        
-        //return message.channel.send("Message: " + messages[Math.floor(Math.random()*messages.length)].toString());
+        message.channel.messages.fetch(Snowflake.fromDate(DateGenerator.getRandomDateInRange(startDate, endDate)))
+        .then(m => message.channel.send(m.author.username + ": \"" + m.toString() + "\""))
+        .catch(console.error);
 	},
 };
+
