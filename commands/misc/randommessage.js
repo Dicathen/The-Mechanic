f704dbc51c2084e4b9d781fddc99863a4be02f14
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-15 17:03:50 -0700

    s

--- commands/misc/randommessage.js
+++ commands/misc/randommessage.js
@@ -1,32 +1,20 @@
-
-let messages = [];
-let lookingForMessages = false;
+const fs = require('fs');
 module.exports = {
 	name: "randommessage",
 	aliases: ["rm", "randmes"],
 	description: "Sends a random message from a the discord",
 
 	async execute(message, args) {
-        if(lookingForMessages)
-        {
-            return message.reply("I am looking for messages.");
-        }
-
         message.channel.send("Trying to fetch all messages please don't send me another command.");
-		if(messages.length < 1)
-        {
-            await module.exports.fetchAllMessages(message.channel);
-            lookingForMessages = false;
-        }
+
+        const channelMessages = await message.channel.messages.fetch({ limit: 10 }).then(() => {
+            fs.writeFile('messages.txt', channelMessages, err => {
+                if (err) {
+                  console.error(err);
+                }
+              });
+        })
         
-        return message.channel.send("Message: " + messages[Math.floor(Math.random()*messages.length)].toString());
+        //return message.channel.send("Message: " + messages[Math.floor(Math.random()*messages.length)].toString());
 	},
-    lookForMessages()
-    {
-        lookingForMessages = true;
-    },
-    doneLookingForMessages()
-    {
-        lookingForMessages = false;
-    }
 };
