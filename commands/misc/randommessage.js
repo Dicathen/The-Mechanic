d6a157da09fd4be3161a79103019f530eb0c083f
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-15 19:13:09 -0700

    Update randommessage.js

--- commands/misc/randommessage.js
+++ commands/misc/randommessage.js
@@ -10,7 +10,7 @@ module.exports = {
         let startDate = new Date(2020, 10, 15);
         let endDate = new Date(); // should be current date
 
-        let snow = fromDate(new Date(2020, 10, 16));
+        let snow = new Snowflake(new Date(2020, 10, 16));
         console.log(snow.toString());
         message.channel.messages.fetch(snow.toString())
         .then(m => message.channel.send(m.content))
@@ -18,10 +18,114 @@ module.exports = {
 	},
 };
 
-function fromDate(date) {
-    return BigInt(`0b${BigInt(date.getTime() - EPOCH) // Translate the date by the Discord epoch.
-        .toString(2) // Convert the date to binary.
-        .padStart(0x2a, "0") // Pad the start until the timestamp is the correct length.
-        .padEnd(0x40, "0") // Pad the end until the snowflake is the correct length.
-    }`);
+class Snowflake {
+	static EPOCH = 1420070400000; // First second of 2015.
+
+	static BITS = {
+		TIMESTAMP: 42,
+		WORKER: 5,
+		PROCESS: 5,
+		INCREMENT: 12
+	};
+
+	static #dateToSnowflake = (date) => {
+			let translatedDate = BigInt(date.getTime() - Snowflake.EPOCH);
+
+			// Convert date to binary.
+			let output = '';
+			while (translatedDate > 0) {
+				output = `${translatedDate % 2n}${output}`;
+				translatedDate /= 2n;
+			}
+
+			// Fill 0s to make 64-bit
+			while (output.length < Snowflake.BITS.TIMESTAMP) {
+				output = `0${output}`;
+			}
+			while (output.length < Object.keys(Snowflake.BITS)
+				   .reduce((a, c) => a + parseInt(Snowflake.BITS[c] || 0), 0)) {
+				output += '0';
+			}
+
+			return BigInt(`0b${output}`);
+	};
+
+	#value; // BigInt representation of Snowflake.
+
+	constructor(value = new Date()) {
+		if (
+			typeof value == 'number'
+			|| typeof value == 'bigint'
+			|| typeof value == 'string' && !isNaN(parseInt(value))
+		) {
+			this.#value = BigInt(value);
+		} else if (value instanceof Date) {
+			this.#value = Snowflake.#dateToSnowflake(value);
+		} else {
+			throw new Error(`Cannot create a Snowflake from supplied value (${value}).`);
+		}
+	}
+
+	get binary() {
+		let decimal = this.#value;
+		let binary = '';
+
+		// Convert to binary.
+		while (decimal > 0) {
+			binary = `${decimal % 2n}${binary}`;
+			decimal /= 2n;
+		}
+
+		// Fill 0s to make 64-bit.
+		while (binary.length < Object.keys(Snowflake.BITS)
+			   .reduce((a, c) => a + parseInt(Snowflake.BITS[c] || 0), 0)) {
+			binary = `0${binary}`;
+		}
+
+		return binary;
+	}
+
+	get timestamp() {
+		return new Date(parseInt(
+			this.binary.substring(
+				0,
+				
+				Snowflake.BITS.TIMESTAMP
+			), 2) + Snowflake.EPOCH);
+	}
+
+	get worker() {
+		return parseInt(
+			this.binary.substring(
+				Snowflake.BITS.TIMESTAMP,
+				
+				Snowflake.BITS.TIMESTAMP +
+				Snowflake.BITS.WORKER
+			), 2);
+	}
+
+	get process() {
+		return parseInt(
+			this.binary.substring(
+				Snowflake.BITS.TIMESTAMP +
+				Snowflake.BITS.WORKER,
+				
+				Snowflake.BITS.TIMESTAMP +
+				Snowflake.BITS.WORKER +
+				Snowflake.BITS.PROCESS
+			), 2);
+	}
+
+	get increment() {
+		return parseInt(
+			this.binary.substring(
+				Snowflake.BITS.TIMESTAMP +
+				Snowflake.BITS.WORKER +
+				Snowflake.BITS.PROCESS
+			), 2);
+	}
+
+	toString() {
+		return `${this.#value}`;
+	}
 }
