0b877c2fe8e4ef7060e3f334cac88dd659c3dd28
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-15 17:10:34 -0700

    Update randommessage.js

--- commands/misc/randommessage.js
+++ commands/misc/randommessage.js
@@ -7,13 +7,16 @@ module.exports = {
 	async execute(message, args) {
         message.channel.send("Trying to fetch all messages please don't send me another command.");
 
+        var file = fs.createWriteStream('messages.txt')
+        file.on('error', function(err) { /* error handling */ });
         await message.channel.messages.fetch({ limit: 10 }).then(messages => {
-            fs.writeFile('messages.txt', messages, err => {
-                if (err) {
-                  console.error(err);
-                }
-              });
+            messages.forEach(function(m) { 
+                file.write(m.author.username + ": \"" + m.toString() + "\"");
+            });
         })
+
+        arr.forEach(function(v) { file.write(v.join(', ') + '\n'); });
+        file.end();
         
         //return message.channel.send("Message: " + messages[Math.floor(Math.random()*messages.length)].toString());
 	},
