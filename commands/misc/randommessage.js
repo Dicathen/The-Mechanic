76aa17229927a022ddc1f8bebc18d8c2e3c226bc
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-10-15 19:31:00 -0700

    Update randommessage.js

--- commands/misc/randommessage.js
+++ commands/misc/randommessage.js
@@ -6,7 +6,7 @@ module.exports = {
 
 	async execute(message, args) {
         let snow = new Snowflake(new Date(Random(2020, 2022), Random(1, new Date().getMonth()), Random(1, 29), Random(1, 24), Random(1, 60), Random(1, 60), Random(1, 60)));
-        console.log(snow);
+        console.log(snow.toString());
         message.channel.messages.fetch(snow)
         .then(m => message.channel.send(m.content))
         .catch(console.error);
