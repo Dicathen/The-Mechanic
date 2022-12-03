8e5e6a01fd83c9b9343c61e0fd1d4bfeb05e1869
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-12-02 17:49:07 -0800

    Update onMention.js

--- messages/onMention.js
+++ messages/onMention.js
@@ -19,6 +19,7 @@ module.exports = {
 	 */
 
 	async execute(message) {
+        console.log("Do i get here?");
         var prompt = `You: ${message.content.substring(22)}\n`;
         console.log(prompt);
         (async () => {
