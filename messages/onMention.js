db90ac466c70c0c06f355339d12ffe1571278803
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-12-02 19:06:15 -0800

    Update onMention.js

--- messages/onMention.js
+++ messages/onMention.js
@@ -19,7 +19,7 @@ module.exports = {
 	 */
 
 	async execute(message) {
-        var prompt = `Respond as if you're a bubbly, sarcastic, flirty tomboy who hates Nicholas Greenwood.\n${message.content.substring(22)}\n`;
+        var prompt = `Respond as if you're a bubbly, flirty, snarky tomboy.\n${message.content.substring(22)}\n`;
         (async () => {
             const gptResponse = await openai.createCompletion({
                 model: "text-davinci-003",
