0b7a304215ac51304bdc9905662140069f954f19
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-12-02 16:58:00 -0800

    s

--- messages/onMention.js
+++ messages/onMention.js
@@ -4,7 +4,7 @@
  * @since 3.0.0
  */
 
-const { prefix } = require("../config.json");
+const { prefix, OPENAI_SECRET_KEY } = require("../config.json");
 
 module.exports = {
 	/**
@@ -14,8 +14,30 @@ module.exports = {
 	 */
 
 	async execute(message) {
+        var prompt = message.content.substring(22);
+        //var response = await generate(message.content)
+        console.log(message.content);
 		return message.channel.send(
 			`Hi ${message.author}! My prefix is \`${prefix}\`, get help by \`${prefix}help\``
 		);
 	},
 };
+
+const got = require('got');
+
+async function generate(prompt) {
+  const url = 'https://api.openai.com/v1/engines/davinci/completions';
+  const params = {
+    "prompt": prompt,
+    "max_tokens": 160,
+    "temperature": 0.7,
+    "frequency_penalty": 0.5
+  };
+  const headers = {
+    'Authorization': `Bearer ${process.env.OPENAI_SECRET_KEY}`,
+  };
+
+  const response = await got.post(url, { json: params, headers: headers }).json();
+  output = `${prompt}${response.choices[0].text}`;
+  return output;
+}
