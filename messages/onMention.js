1f50059fe892a79e7a4cfb4caecd1c71d27823c8
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-12-03 21:10:31 -0800

    Update onMention.js

--- messages/onMention.js
+++ messages/onMention.js
@@ -30,7 +30,7 @@ module.exports = {
                 temperature: 0.6,
                 max_tokens: 1024,
               });
-            message.reply(`${index}: ${gptResponse.data.choices[0].text}`);
+            message.reply(`${gptResponse.data.choices[0].text}`);
 
         })();
 	},
