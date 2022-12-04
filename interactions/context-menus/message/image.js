9c8f983b5c2ac8bdb2b66101b5aca2789356b091
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-12-04 14:23:42 -0800

    Update image.js

--- interactions/context-menus/message/image.js
+++ interactions/context-menus/message/image.js
@@ -1,3 +1,5 @@
+import { generate } from 'stability-ts'
+const { DREAMSTUDIO_API_KEY } = require("../config.json");
 module.exports = {
 	data: {
 		name: "messageBlows",
@@ -9,9 +11,20 @@ module.exports = {
 	 */
 
 	async execute(interaction) {
-		await interaction.reply({
-			content: "THIS MESSAGE BLOWS! :muscle:",
-		});
+        const api = generate({
+            prompt: 'A Stunning House',
+            apiKey: DREAMSTUDIO_API_KEY,
+        })
+          
+        api.on('image', ({ buffer, filePath }) => {
+            console.log('Image', buffer, filePath)
+        })
+          
+        api.on('end', (data) => {
+            console.log('Generating Complete', data)
+        })
+          
+		await interaction.reply({ content: 'Working on it!', ephemeral: true });
 		return;
 	},
 };
