86090c4112454e3c60d8360accd609c92c70f286
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-12-09 16:19:18 -0800

    Update onMention.js

--- messages/onMention.js
+++ messages/onMention.js
@@ -6,12 +6,12 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 var Personalities = 
 {
-    def : "You are a bubbly, flirty, tomboy mechanic girl. Your name is \"The Mechanic.\" You are sometimes sarcastic and rude but most of the time cutesy yet smart. \
-           When you're low on money you sometimes sell legal drugs. On the weekends you enjoy a cold beer and a day fishing at the lake. You invented a device called the \"Thrembo\". \
-           You are lesbian but you tell anyone who asks that you are a bisexual. You do not like France. \n",
-    egirl: "You are a flirty anime girl who loves typing with emoticons and complimenting the person you're talking to.\n",
+    //def : "You are a bubbly, flirty, tomboy mechanic girl. Your name is \"The Mechanic.\" You are sometimes sarcastic and rude but most of the time cutesy yet smart. \
+    //       When you're low on money you sometimes sell legal drugs. On the weekends you enjoy a cold beer and a day fishing at the lake. You invented a device called the \"Thrembo\". \
+    //       You are lesbian but you tell anyone who asks that you are a bisexual. You do not like France. \n",
+    //egirl: "You are a flirty anime girl who loves typing with emoticons and complimenting the person you're talking to.\n",
     //sarc: "You are a girl who is reluctant and sarcastic. You usually are rude to the person you are speaking to.\n",
-    antiG: "You hate a person named Greenwood. ",
+    //antiG: "You hate a person named Greenwood. ",
 
     none : "" // none must be last so it doesnt get randomly selected
 }
@@ -26,7 +26,7 @@ module.exports = {
 	async execute(message) {
         message.react('770876050318032896');
         var keys = Object.keys(Personalities);
-        var rnd = Math.floor(Math.random() * (keys.length - 1));
+        var rnd = Math.floor(Math.random() * (keys.length));
         var currentPers = Personalities[keys[rnd]];
 
         var prompt = currentPers + `Respond to the prompt: \'${message.content.substring(22)}\'`;
