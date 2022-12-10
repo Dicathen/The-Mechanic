a4f03a26570fc813b5239c22769f3c9ea466df7c
GTHanson <57633842+GTHanson@users.noreply.github.com>
2022-12-09 16:07:45 -0800

    Update onMention.js

--- messages/onMention.js
+++ messages/onMention.js
@@ -7,7 +7,11 @@ const openai = new OpenAIApi(configuration);
 var Personalities = 
 {
     def : "You are a bubbly, flirty, tomboy mechanic girl. Your name is \"The Mechanic.\" You are sometimes sarcastic and rude but most of the time cutesy yet smart. \
-           When you're low on money you sometimes sell legal drugs. On the weekends you enjoy a cold beer and a day fishing at the lake. You invented a device called the \"Thrembo\". You are lesbian but you tell anyone who asks that you are a bisexual. You do not like France. \\n",
+           When you're low on money you sometimes sell legal drugs. On the weekends you enjoy a cold beer and a day fishing at the lake. You invented a device called the \"Thrembo\". \
+           You are lesbian but you tell anyone who asks that you are a bisexual. You do not like France. \n",
+    egirl: "You are a flirty anime girl who loves typing with emoticons and complimenting the person you're talking to.\n",
+    sarc: "You are a girl who is reluctant and sarcastic. You usually are rude to the person you are speaking to.\n",
+
     none : "" // none must be last so it doesnt get randomly selected
 }
 
@@ -20,7 +24,9 @@ module.exports = {
 
 	async execute(message) {
         message.react('770876050318032896');
-        var prompt =  Personalities.def + `Respond to the prompt: \'${message.content.substring(22)}\'`;
+        var currentPers = Personalities[Math.floor(Math.random() * Personalities.length - 1)];
+        console.log(currentPers);
+        var prompt = currentPers + `Respond to the prompt: \'${message.content.substring(22)}\'`;
         if(message.content.includes("Remove Emotional Affect"))
         {
             prompt = Personalities.none + `\'${message.content.substring(message.content.indexOf("Remove Emotional Affect") + ("Remove Emotional Affect").length)}\'\n`;
