3d6f61535eb194911a47298f271b2a0a3b7c8c4a
Ty Morrow <morrowtym@gmail.com>
2023-01-28 21:14:06 -0600

    Update jayden.js

--- commands/misc/jayden.js
+++ commands/misc/jayden.js
@@ -7,8 +7,11 @@ module.exports = {
 		let count = 0;
 		for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
 		var messageText = "";
-		var date = new Date().getDay();
-		var time = new Date().getHours();
+		var date = new Date();
+		//Change the time to match CST rather than UTC
+		date.setHours(date.getHours() - 6);
+		var day = date.getDay();
+		var time = date.getHours();
 		var tod = "";
 		if(time >= 0 && time < 12) {
 			tod = "morning";
@@ -21,13 +24,13 @@ module.exports = {
 		}
 
 		if(count <= 4) {
-			messageText = "Damn, chat's really dead on a " + date + " " + tod + " huh?";
+			messageText = "Damn, chat's really dead on a " + day + " " + tod + " huh?";
 		}
 		else if(count > 4 && count <= 8) {
-			messageText = "Chat's really popping off on a " + date + " " + tod + ".";
+			messageText = "Chat's really popping off on a " + day + " " + tod + ".";
 		}
 		else {
-			messageText = "Wowzers! Chat's really super EXTRA popping off on a " + date + " " + tod + "!";
+			messageText = "Wowzers! Chat's really super EXTRA popping off on a " + day + " " + tod + "!";
 		}
 		message.channel.send({ content: messageText});
 	},
