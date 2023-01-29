5264b46a4517a8ac113362620bd2132bea5f4e26
Ty Morrow <morrowtym@gmail.com>
2023-01-28 21:18:48 -0600

    Update jayden.js

--- commands/misc/jayden.js
+++ commands/misc/jayden.js
@@ -3,15 +3,29 @@ module.exports = {
 	description: "Gets Jayden's impression of the server",
 
 	execute(message, args) {
+		var messageText = "";
+
+		//Get number of people in the voice channels.
 		const voiceChannels = message.guild.channels.cache.filter(c => c.type === 'voice');
 		let count = 0;
 		for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
-		var messageText = "";
+		console.log(count);
+
+		//Set up days of the week.
+		var weekday = new Array(7);
+		weekday[0] = "Monday";
+		weekday[1] = "Tuesday";
+		weekday[2] = "Wednesday";
+		weekday[3] = "Thursday";
+		weekday[4] = "Friday";
+		weekday[5] = "Saturday";
+		weekday[6] = "Sunday";
+
+		//Set up time of day.
 		var date = new Date();
-		//Change the time to match CST rather than UTC
 		date.setHours(date.getHours() - 6);
 		var day = date.getDay();
-		var time = date.getHours();
+		var time = weekday[date.getHours()];
 		var tod = "";
 		if(time >= 0 && time < 12) {
 			tod = "morning";
