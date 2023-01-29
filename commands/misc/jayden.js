274d62f3744d5cedfe1fc67fa7c12970d8c364f4
Ty Morrow <morrowtym@gmail.com>
2023-01-28 22:03:41 -0600

    Update jayden.js

--- commands/misc/jayden.js
+++ commands/misc/jayden.js
@@ -13,7 +13,7 @@ module.exports = {
 		//console.log(allChannels);
 		let count = 0;
 		for (const [id, voiceChannel] of voiceChannels) {
-			count += voiceChannel.members.size;
+			count += (voiceChannel.members.size % 8);
 			console.log(count);
 		}
 		console.log(count);
